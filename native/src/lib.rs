0c0aaf424a2cc2683376ce201c0bbb02ec43d1d8
Taher <entrptaher@gmail.com>
2020-04-10 20:29:47 +0600

    fix: add more options

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -2,12 +2,16 @@ extern crate serde;
 extern crate serde_json;
 
 use neon::prelude::*;
-use neon_serde;
 
 #[macro_use]
 extern crate serde_derive;
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use serde_json::{json, Value};
+
+#[macro_use]
+extern crate neon;
+#[macro_use]
+extern crate neon_serde;
 
 #[derive(Serialize, Deserialize)]
 struct Link {
@@ -16,32 +20,33 @@ struct Link {
     stuff: Value,
 }
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsValue> {
-    // read arguments and turn them into json
-    let mut list = vec![];
-    let arg0 = cx.argument::<JsValue>(0)?;
-    let object: serde_json::Value = neon_serde::from_value(&mut cx, arg0)?;
-    for (page_key, page) in object.as_object().unwrap().iter() {
-        for (group_key, group) in page.as_object().unwrap().iter() {
-            for stuff in group.as_array().unwrap() {
-                // println!("{:#?}", stuff);
-                let link = Link {
-                    page_key: page_key.to_string(),
-                    group_key: group_key.to_string(),
-                    stuff: stuff.clone(),
-                };
-                list.push(link);
+export! {
+    fn hellojson() -> String {
+        let jsondata = json!({"page1":{"group1":[{"name":"name1","index":1},{"name":"name33","index":33},{"name":"name45","index":45}]},"page500":{"group25":[{"name":"name1","index":1},{"name":"name33","index":33},{"name":"name45","index":45}]}});
+        let result = serde_json::to_string(&jsondata).unwrap();
+        return format!("{}", result);
+    }
+    fn hello2(input: String) -> String {
+        let object: Value = serde_json::from_str(&input).unwrap();
+        let result = serde_json::to_string(&object).unwrap();
+        return format!("{}", result);
+    }
+    fn hello(input: String) -> String {
+        let mut list = vec![];
+        let object: Value = serde_json::from_str(&input).unwrap();
+        for (page_key, page) in object.as_object().unwrap().iter() {
+            for (group_key, group) in page.as_object().unwrap().iter() {
+                for stuff in group.as_array().unwrap() {
+                    let link = Link {
+                        page_key: page_key.to_string(),
+                        group_key: group_key.to_string(),
+                        stuff: stuff.clone(),
+                    };
+                    list.push(link);
+                }
             }
         }
+        let result = serde_json::to_string(&list).unwrap();
+        return format!("{}", result);
     }
-
-    // do stuff with the object
-    // println!("{:#?}", object);
-
-    // return back to nodejs
-    let result = neon_serde::to_value(&mut cx, &list)?;
-    Ok(result)
-    // Ok(cx.undefined().upcast())
 }
-
-register_module!(mut cx, { cx.export_function("hello", hello) });
