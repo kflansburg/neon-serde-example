b72eb7684904fc951978efab5024e7380469ef29
Kevin Flansburg <kevin.flansburg@gmail.com>
2020-04-10 11:20:16 -0500

    Attempt

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,42 +1,126 @@
 extern crate serde;
 extern crate serde_json;
 
+use std::fmt;
+
+use serde::de::{Visitor, Deserializer};
 use neon::prelude::*;
 use neon_serde;
 
 #[macro_use]
 extern crate serde_derive;
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use serde::{Deserialize};
+
+#[derive(Serialize, Deserialize, Clone, Copy)]
+struct Data {
+    name: Name,
+    index: usize
+}
 
 #[derive(Serialize, Deserialize)]
 struct Link {
-    page_key: String,
-    group_key: String,
-    stuff: Value,
+    page: Page,
+    group: Group,
+    stuff: Data
 }
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsValue> {
-    // read arguments and turn them into json
-    let mut list = vec![];
-    let arg0 = cx.argument::<JsValue>(0)?;
-    let object: serde_json::Value = neon_serde::from_value(&mut cx, arg0)?;
-    for (page_key, page) in object.as_object().unwrap().iter() {
-        for (group_key, group) in page.as_object().unwrap().iter() {
-            for stuff in group.as_array().unwrap() {
-                // println!("{:#?}", stuff);
-                let link = Link {
-                    page_key: page_key.to_string(),
-                    group_key: group_key.to_string(),
-                    stuff: stuff.clone(),
-                };
-                list.push(link);
-            }
-        }
+#[derive(Serialize, PartialOrd, Ord, PartialEq, Eq, Clone, Copy)]
+struct Name(usize);
+
+#[derive(Serialize, PartialOrd, Ord, PartialEq, Eq, Clone, Copy)]
+struct Group(usize);
+
+#[derive(Serialize, PartialOrd, Ord, PartialEq, Eq, Clone, Copy)]
+struct Page(usize);
+
+struct GroupVisitor;
+
+impl<'de> Visitor<'de> for GroupVisitor {
+    type Value = Group;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("an group name")
     }
 
-    // do stuff with the object
-    // println!("{:#?}", object);
+    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E> {
+        Ok(Group(value[5..].parse().unwrap()))
+    }
+}
+
+impl<'de> Deserialize<'de> for Group {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_str(GroupVisitor)
+    }
+}
+
+struct PageVisitor;
+
+impl<'de> Visitor<'de> for PageVisitor {
+    type Value = Page;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("an page name")
+    }
+
+    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E> {
+        Ok(Page(value[4..].parse().unwrap()))
+    }
+}
+
+impl<'de> Deserialize<'de> for Page {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_str(PageVisitor)
+    }
+}
+
+struct NameVisitor;
+
+impl<'de> Visitor<'de> for NameVisitor {
+    type Value = Name;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("an name")
+    }
+
+    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E> {
+        Ok(Name(value[4..].parse().unwrap()))
+    }
+}
+
+impl<'de> Deserialize<'de> for Name {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_str(NameVisitor)
+    }
+}
+
+#[derive(Deserialize)]
+struct ObjectTuple<K,V>(#[serde(with = "tuple_vec_map")] Vec<(K,V)>) where K: serde::de::DeserializeOwned, V: serde::de::DeserializeOwned;
+
+fn hello(mut cx: FunctionContext) -> JsResult<JsValue> {
+    let arg0: String = cx.argument::<JsString>(0)?.value();
+
+    let object : ObjectTuple<Page, ObjectTuple<Group, Vec<Data>>> = serde_json::from_str(& arg0).unwrap();
+
+    let list: Vec<Link> = object.0.iter().map(|(page_key, page)| {
+        page.0.iter().map(move |(group_key, group)| {
+           group.iter().map(move |data| {
+               Link {
+                   stuff: *data,
+                   page: *page_key,
+                   group: *group_key
+               }
+           })
+        })
+    }).flatten().flatten().collect();
 
     // return back to nodejs
     let result = neon_serde::to_value(&mut cx, &list)?;
